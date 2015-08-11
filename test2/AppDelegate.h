1d76554794927830015563eaff0d918e139cff52
Chaeyoung-Youn <cyoun@cafcloud.com>
2015-08-11 11:28:06 +0900

    Initial Commit

---
+++ test2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test2
+//
+//  Created by Chaeyoung Yoon on 8/11/15.
+//  Copyright (c) 2015 Chaeyoung Yoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test2
+//
+//  Created by Chaeyoung Yoon on 8/11/15.
+//  Copyright (c) 2015 Chaeyoung Yoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
